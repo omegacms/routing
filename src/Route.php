ca66772a6b4ebe9f3741611a9c8b3e705e85aa91
omegacms <omegacms@outlook.com>
2023-12-23 13:15:44 +0100

    Update Site Url

--- src/Route.php
+++ src/Route.php
@@ -39,9 +39,9 @@ use Omega\Helpers\App;
  *
  * @category    Omega
  * @package     Omega\Routing
- * @link        https://omegacms.github.com
+ * @link        https://omegacms.github.io
  * @author      Adriano Giovannini <omegacms@outlook.com>
- * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.com)
+ * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
  * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  * @version     1.0.0
  */
--- src/RouterInterface.php
+++ src/RouterInterface.php
@@ -23,9 +23,9 @@ namespace Omega\Routing;
  *
  * @category    Omega
  * @package     Omega\Routing
- * @link        https://omegacms.github.com
+ * @link        https://omegacms.github.io
  * @author      Adriano Giovannini <omegacms@outlook.com>
- * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.com)
+ * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
  * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  * @version     1.0.0
  */
